b265953e399fbb62edea98c4cd2cbc9113e8b071
KIra50Hh <antoine_Arnoud@etu.u-bourgogne.fr>
2022-11-30 13:55:34 +0100

    Question 4
    Ajout de la dernière requête SQL

--- src/Services/Hotel/UnoptimizedHotelService.php
+++ src/Services/Hotel/UnoptimizedHotelService.php
@@ -135,54 +135,30 @@ class UnoptimizedHotelService extends AbstractHotelService {
    * @return RoomEntity
    */
   protected function getCheapestRoom ( HotelEntity $hotel, array $args = [] ) : RoomEntity {
-    // On charge toutes les chambres de l'hôtel
-    $query = "SELECT * FROM wp_posts JOIN wp_postmeta ON wp_posts.ID = wp_postmeta.post_id WHERE post_author = :hotelID AND post_type = 'room'";
 
-    $whereClauses = [];
-
-    if (isset($args['surface']['min']))
-        $whereClauses[] = 'surface >= :surfaceMin';
-
-    if (isset($args['surface']['max']))
-        $whereClauses[] = 'surface <= :surfaceMax';
-
-    if (isset($args['price']['min']))
-        $whereClauses[] = 'price >= :priceMin';
-
-    if (isset($args['price']['max']))
-        $whereClauses[] = 'price <= :priceMax';
-
-    if (isset($args['rooms']))
-        $whereClauses[] = 'rooms => :rooms';
-
-    if (isset($args['bathRooms']))
-        $whereClauses[] = 'bathRooms => :bathRooms';
-
-    if (count($whereClauses) > 0)
-        $query .= ' AND ' . implode(' AND ', $whereClauses);
-
-    $stmt = $this->getDB()->prepare( $query );
-
-    if (isset($args['surface']['min']))
-        $stmt->bindValue(':surfaceMin', $args['surface']['min'], PDO::PARAM_INT);
-
-    if (isset($args['surface']['max']))
-        $stmt->bindValue(':surfaceMax', $args['surface']['max'], PDO::PARAM_INT);
-
-    if (isset($args['price']['min']))
-        $stmt->bindValue(':priceMin', $args['price']['min'], PDO::PARAM_INT);
-
-    if (isset($args['price']['max']))
-        $stmt->bindValue(':priceMax', $args['price']['max'], PDO::PARAM_INT);
-
-    if (isset($args['rooms']))
-        $stmt->bindValue(':rooms', $args['rooms'], PDO::PARAM_INT);
-
-    if (isset($args['bathRooms']))
-        $stmt->bindValue(':bathRooms', $args['bathRooms'], PDO::PARAM_INT);
-
-    $stmt->bindValue(':hotelID', $hotel->getId(), PDO::PARAM_INT);
-    $stmt->execute();
+    $whereClause = [];
+    if ( isset( $args['surface']['min'] )  )
+      $whereClause[] = 'surfaceData.meta_value >= ' . $args['surface']['min'];
+    if ( isset( $args['surface']['max'] )  )
+      $whereClause[] = 'surfaceData.meta_value <= ' . $args['surface']['max'];
+    if ( isset( $args['price']['min'] ) )
+      $whereClause[] = 'priceData.meta_value >= ' . $args['price']['min'];
+    if ( isset( $args['price']['max'] ) )
+      $whereClause[] = 'priceData.meta_value <= ' . $args['price']['max'];
+    if ( isset( $args['rooms'] )  )
+      $whereClause[] = 'roomsData.meta_value  >= ' . $args['rooms'];
+    if ( isset( $args['bathRooms'] ) )
+      $whereClause[] = 'bathRoomsData.meta_value >= ' . $args['bathRooms'];
+    if ( isset( $args['types'] ) && ! empty( $args['types'] )  )
+      $whereClause[] = 'typeData.meta_value IN ("' . implode( '","', $args['types'] ) . '")';
+    $stmt = $this->getDB()->prepare( "SELECT * FROM wp_posts 
+    INNER JOIN wp_postmeta as surfaceData ON surfaceData.post_id = wp_posts.ID AND surfaceData.meta_key = 'surface' 
+    INNER JOIN wp_postmeta as priceData ON priceData.post_id = wp_posts.ID AND priceData.meta_key = 'price'
+    INNER JOIN wp_postmeta as roomsData ON roomsData.post_id = wp_posts.ID AND roomsData.meta_key = 'bedrooms_count' 
+    INNER JOIN wp_postmeta as bathRoomsData ON bathRoomsData.post_id = wp_posts.ID AND bathRoomsData.meta_key = 'bathrooms_count'
+    INNER JOIN wp_postmeta as typeData ON typeData.post_id = wp_posts.ID AND typeData.meta_key = 'type'    
+    WHERE post_author = :hotelId AND post_type = 'room'" . ( ! empty( $whereClause ) ? ' AND ' . implode( ' AND ', $whereClause ) : '' ) . " ORDER BY priceData.meta_value ASC LIMIT 1" );
+    $stmt->execute( [ 'hotelId' => $hotel->getId() ] ); 
     $filteredRooms = $stmt->fetchAll( PDO::FETCH_ASSOC );
     
     // Si aucune chambre ne correspond aux critères, alors on déclenche une exception pour retirer l'hôtel des résultats finaux de la méthode list().
